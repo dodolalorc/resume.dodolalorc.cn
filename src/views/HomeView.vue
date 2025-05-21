72acc784885f8b9ab1a8b20700ff0a5382ca8d2f
Flora Chen <chenflora124@gmail.com>
2025-05-21 21:08:08 +0800

    🔥 Remove init files and add toml support

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted,ref } from 'vue';
+import * as toml from 'toml'
+
+interface ServerConfig {
+  host: string
+  port: number
+}
+
+interface FeaturesConfig {
+  dark_mode: boolean
+  analytics: boolean
+}
+
+interface AppConfig {
+  title: string
+  author: string
+  description: string
+  server: ServerConfig
+  features: FeaturesConfig
+}
+
+const config = ref<Partial<AppConfig>>({})
+onMounted(async () => {
+  try {
+    const response = await fetch('/resume.toml')
+    const text = await response.text()
+    config.value = toml.parse(text) as AppConfig
+  } catch (error) {
+    console.error('Error loading config:', error)
+  }
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+  <h1>{{ config }}</h1>
+  </div>
 </template>
