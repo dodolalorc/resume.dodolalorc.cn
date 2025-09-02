42fe9f260b62a266bf26905319424da80d9c6180
chenjing <chenjing.ddl@bytedance.com>
2025-09-02 16:00:03 +0800

    chore(cv.json): update cv.json

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ResumeView from '../views/ResumeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'resume',
+      component: ResumeView,
     },
   ],
 })
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-import * as toml from "toml";
 import type { AppConfig } from "@/types/appConfig";
 
 const config = ref<Partial<AppConfig>>({});
 onMounted(async () => {
 	try {
-		const response = await fetch("/resume.toml");
-		const text = await response.text();
-		config.value = toml.parse(text) as AppConfig;
+		const response = await fetch("/cv.json");
+		const data = await response.json();
+		config.value = data as AppConfig;
 	} catch (error) {
 		console.error("Error loading config:", error);
 	}
